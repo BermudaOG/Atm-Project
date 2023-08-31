edd4dbc5ebd83b1cd5f5c16d0258ead18d011dce
jose <torrj755@gmail.com>
2023-08-30 18:24:39 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Account from 'Account';
 
 function App() {
   return (
     <div className="App">
+      <div className="left-column"></div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Account />
       </header>
+      <div className="right-column"></div>
     </div>
   );
 }
